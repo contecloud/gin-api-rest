b97436b1b0554ccd11702269d270e52cc52e0eff
contecloud <carlo.conte@gmail.com>
2022-03-14 20:40:54 -0300

    Implementa busca e busca por ID

--- main.go
+++ main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"github.com/contecloud/gin-api-rest/database"
-	"github.com/contecloud/gin-api-rest/models"
 	"github.com/contecloud/gin-api-rest/routes"
 )
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Carlo Conte", CPF: "01199294063", RG: "5085403946"},
-		{Nome: "Ana", CPF: "01312312323", RG: "505216"},
-		{Nome: "Sérgio", CPF: "32132132121", RG: "123465479"},
-	}
 	routes.HandleRequests()
 }
