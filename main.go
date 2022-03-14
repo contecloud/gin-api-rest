13d745135110bbd45ffc515cd08c3b1d6c4f5011
contecloud <carlo.conte@gmail.com>
2022-03-14 19:46:47 -0300

    Implementa conexao com base de dados

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/contecloud/gin-api-rest/database"
 	"github.com/contecloud/gin-api-rest/models"
 	"github.com/contecloud/gin-api-rest/routes"
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "Carlo Conte", CPF: "01199294063", RG: "5085403946"},
 		{Nome: "Ana", CPF: "01312312323", RG: "505216"},
