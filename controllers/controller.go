8a4de7d604da8847037dea9671ac12b5dbfe120a
F1728819 Carlo Conte <carlo.conte@bb.com.br>
2022-03-14 14:03:52 -0300

    Acrescenta model e estrutura tudo

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,10 +1,17 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/contecloud/gin-api-rest/models"
+	"github.com/gin-gonic/gin"
+)
 
 func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, models.Alunos)
+}
+
+func Saudacao(c *gin.Context) {
+	nome := c.Params.ByName("nome")
 	c.JSON(200, gin.H{
-		"id":   "1",
-		"nome": "Carlo Conte",
+		"API diz: ": "E ai " + nome + ", tudo beleza?",
 	})
 }
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/contecloud/gin-api-rest/routes"
+import (
+	"github.com/contecloud/gin-api-rest/models"
+	"github.com/contecloud/gin-api-rest/routes"
+)
 
 func main() {
+	models.Alunos = []models.Aluno{
+		{Nome: "Carlo Conte", CPF: "01199294063", RG: "5085403946"},
+		{Nome: "Ana", CPF: "01312312323", RG: "505216"},
+		{Nome: "Sérgio", CPF: "32132132121", RG: "123465479"},
+	}
 	routes.HandleRequests()
 }
