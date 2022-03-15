330bb04a871fd1d18f758f48363022fa4a8d0169
contecloud <carlo.conte@gmail.com>
2022-03-14 21:16:36 -0300

    Finalizando o projeto com busca por CPF

--- controllers/controller.go
+++ controllers/controller.go
@@ -67,3 +67,16 @@ func EditaAluno(c *gin.Context) {
 	database.DB.Model(&aluno).UpdateColumns(aluno)
 	c.JSON(http.StatusOK, aluno)
 }
+
+func BuscaAlunoPorCpf(c *gin.Context) {
+	var aluno models.Aluno
+	cpf := c.Param("cpf")
+	database.DB.Where(&models.Aluno{CPF: cpf}).First(&aluno)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not Found": "Aluno não encontrado"})
+		return
+	}
+	c.JSON(http.StatusOK, aluno)
+}
